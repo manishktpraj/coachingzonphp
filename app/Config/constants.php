fd819b63ea277d796e898246e1959b676727c04b
Manish Garg <manishktpraj@gmail.com>
2021-07-26 12:45:27 +0530

    mg

--- app/Config/constants.php
+++ app/Config/constants.php
@@ -4,16 +4,16 @@ if (!defined('DS')) {
 }
 define('ROOT', dirname(__DIR__));
 $strPath ='';
-if(isset($_SERVER['HTTP_HOST']) && $_SERVER['HTTP_HOST']='localhost')
+  ////$_SERVER['HTTP_HOST'];
+if(isset($_SERVER['HTTP_HOST']) && $_SERVER['HTTP_HOST']=='localhost' || isset($_SERVER['HTTP_HOST']) && $_SERVER['HTTP_HOST']=='192.168.1.38')
 {
    $strPath ='coachingzon/';
 }else{
    $strPath ='portal/';
 }
 define('SITE_ABS_PATH',$strPath);
-
-define('SITE_PATH',$_SERVER['DOCUMENT_ROOT'].'/'.SITE_ABS_PATH);
-define('SITE_URL',(($_SERVER['HTTPS']=='on')?'https://':'http://').(isset($_SERVER['HTTP_HOST'])?$_SERVER['HTTP_HOST']:'').'/'.SITE_ABS_PATH);
+  define('SITE_PATH',$_SERVER['DOCUMENT_ROOT'].'/'.SITE_ABS_PATH);
+define('SITE_URL',((isset($_SERVER['HTTPS']) && ($_SERVER['HTTPS']=='on'))?'https://':'http://').(isset($_SERVER['HTTP_HOST'])?$_SERVER['HTTP_HOST']:'').'/'.SITE_ABS_PATH);
  define('ADMIN_URL',SITE_URL.'csadmin/');
 //echo SITE_PATH;die;
 define('ADMIN_ASSETS_URL',SITE_URL.'public/admin-assets/');
